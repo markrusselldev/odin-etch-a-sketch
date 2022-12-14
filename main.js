c42b18e7944b002c565427e3b786ef5285e04702
markrusselldev <mark@markrussell.dev>
2022-12-14 14:19:06 -0700

    style: add css for grid layout, increase loop
    fix: increase loop to 256x to achieve a 16x16 grid

--- main.js
+++ main.js
@@ -3,19 +3,16 @@
 // Each loop, generate a div with class of "box"
 //
 
-// if (typeof document == "undefined") {
-//   window.alert("what the holy fuck batman");
-// }
-
-// const container = document.querySelector("#container");
-
+// create div with class .grid-container
 const container = document.createElement("div");
 container.classList.add("grid-container");
-container.textContent = "Container";
+//container.textContent = "Container";
 
+// add container to body
 document.body.appendChild(container);
 
-for (let i = 0; i < 16; i++) {
+// loop 16x and display .grid-item divs
+for (let i = 0; i < 256; i++) {
   const box = document.createElement("div");
   box.classList.add("grid-item");
   box.textContent = i;
